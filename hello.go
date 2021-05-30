c8b82fb9a356d051b45df8c3ddb40db36571046f
Xihuan Yang <yangxihuan@126.com>
2021-05-30 20:01:59 +0800

    Initial commit for golang hello project

---
+++ hello.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "example.com/hello/greetings"
+    "rsc.io/quote"
+)
+
+func main() {
+    //importing packages from a published module,
+    //quote in https://pkg.go.dev/
+    fmt.Println(quote.Hello())
+    
+    //Use local package
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
+   
+    // A slice of names.
+    names := []string{"Gladys", "Samantha", "Darrin"}
+ 
+    message, err := greetings.Hellos(names)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Println(message)
+}
